ffb813a4aec4c63a0e351c6e965c6ed95234f003
admin@melnikoff.org <admin@melnikoff.org@eef9acde-f33f-11de-af3e-099a1ed3aafd>
2010-02-10 20:26:21 +0000

    Deleted forgotten obj folder.

--- butterfly/obj/network.d
+++
@@ -1,6 +0,0 @@
-./obj/network.o obj/network.d : ../drivers/encx24j600/network.c global-conf.h ../uip/uip.h \
-  ../uip/uipopt.h uip-conf.h apps-conf.h \
-  ../apps/simple-httpd/simple-httpd.h ../uip/psock.h ../uip/pt.h \
-  ../uip/lc.h ../uip/lc-switch.h ../drivers/encx24j600/enc424j600.h \
-  ../drivers/encx24j600/global.h ../drivers/encx24j600/avrlibdefs.h \
-  ../drivers/encx24j600/avrlibtypes.h LCD_driver.h
--- butterfly/obj/simple-httpd.d
+++
@@ -1,4 +0,0 @@
-./obj/simple-httpd.o obj/simple-httpd.d : ../apps/simple-httpd/simple-httpd.c ../uip/uip.h \
-  ../uip/uipopt.h uip-conf.h apps-conf.h \
-  ../apps/simple-httpd/simple-httpd.h ../uip/psock.h ../uip/pt.h \
-  ../uip/lc.h ../uip/lc-switch.h ../apps/simple-httpd/simple-httpd.h
