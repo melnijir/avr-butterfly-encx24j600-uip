2be8450dfd198ab847541bd729df1b64f6a47e9a
admin@melnikoff.org <admin@melnikoff.org@eef9acde-f33f-11de-af3e-099a1ed3aafd>
2010-02-10 20:26:21 +0000

    Deleted forgotten obj folder.

--- trunk/butterfly/obj/uip-neighbor.d
+++
@@ -1,4 +0,0 @@
-./obj/uip-neighbor.o obj/uip-neighbor.d : ../uip/uip-neighbor.c ../uip/uip-neighbor.h ../uip/uip.h \
-  ../uip/uipopt.h uip-conf.h apps-conf.h \
-  ../apps/simple-httpd/simple-httpd.h ../uip/psock.h ../uip/pt.h \
-  ../uip/lc.h ../uip/lc-switch.h
--- trunk/butterfly/obj/uiplib.d
+++
@@ -1,3 +0,0 @@
-./obj/uiplib.o obj/uiplib.d : ../uip/uiplib.c ../uip/uip.h ../uip/uipopt.h uip-conf.h \
-  apps-conf.h ../apps/simple-httpd/simple-httpd.h ../uip/psock.h \
-  ../uip/pt.h ../uip/lc.h ../uip/lc-switch.h ../uip/uiplib.h
